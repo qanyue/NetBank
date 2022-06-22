93fb3a862091d37526fe798902e818fb7b96d062
compass-rose <408437237@qq.com>
2022-06-22 12:54:50 +0800

    变更

--- src/main/java/com/example/servlet/Deposit.java
+++ src/main/java/com/example/servlet/Deposit.java
@@ -1,4 +1,4 @@
-package com.example.demo1;
+package com.example.servlet;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -10,8 +10,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@WebServlet(name = "Withdraw", value = "/Withdraw")
-public class Withdraw extends HttpServlet {
+@WebServlet(name = "Deposit", value = "/Deposit")
+public class Deposit extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
@@ -19,37 +19,33 @@ public class Withdraw extends HttpServlet {
         PrintWriter out=response.getWriter();
         out.println("<meta http-equiv=\"refresh\" content=\"2;URL=User.jsp\">");
         String cardNum = request.getParameter("card_num");
-        String withdrawNum = request.getParameter("withdrawNum");
+        String depositNum = request.getParameter("depositNum");
         String password = request.getParameter("password");
-        String sql = "UPDATE finance.card set card.ca_deposit=card.ca_deposit-"+withdrawNum+" where card.ca_id="+cardNum;
+        String sql = "UPDATE finance.card set card.ca_deposit=card.ca_deposit+"+depositNum+" where card.ca_id="+cardNum;
         String checksql = "select * from finance.card where card.ca_id = '" + cardNum + "' and card.ca_password = '" + password + "';";
         try(Connection conn = GaussDBQuery.getConnetion()) {
             Statement stmt = conn.createStatement();
-            ResultSet rs   = stmt.executeQuery(checksql);
+            ResultSet rs = stmt.executeQuery(checksql);
             if(!rs.next()){
-                 out.println("<h1>输入的账号或密码错误，请重试");
-                 return;
+                out.println("<h1>输入的账号或密码错误，请重试");
+                return;
             }
-            double depositNum = rs.getDouble("ca_deposit");
-            if(Double.parseDouble(withdrawNum) <= depositNum){
-                if(stmt.executeUpdate(sql)>0) {
-                    out.println("<h1>取款成功");
-                } else  {
-                    out.println("<h1>取款失败，清重试");
-                }
-            }else {
-                out.println("<h1>你的余额不足");
-            }
+           if(stmt.executeUpdate(sql)>0) {
+               out.println("<h1>存款成功");
+           } else  {
+               out.println("<h1>存款失败");
+           }
             stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request,response);
+            doGet(request,response);
     }
 }
